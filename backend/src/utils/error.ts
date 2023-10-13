b89d3e2726afd6e8b330b807dd92c48488318742
kaypa <jose_kaypa@hotmail.com>
2023-10-12 20:58:24 -0500

    errorHandler fixed

--- backend/src/utils/error.ts
+++ backend/src/utils/error.ts
@@ -1,5 +1,5 @@
 import { Response } from "express";
 
-export default function errorHandler (statusCode: number = 500, res: Response, message: string = 'Somethig has failed!'){
+export default function errorHandler (res: Response, statusCode: number = 500, message: string = 'Somethig has failed!'){
   res.status(statusCode).json({errorMessage: message});
 }
