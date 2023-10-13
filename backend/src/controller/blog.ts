24dd41f4a676b48317e5f56153017be9064a0d98
kaypa <jose_kaypa@hotmail.com>
2023-10-12 21:35:16 -0500

    controller done

--- backend/src/controller/blog.ts
+++ backend/src/controller/blog.ts
@@ -1,16 +1,46 @@
-import { Request, Response } from "express";
-import Blog from "../model/model";
+import { Request, Response } from 'express'
+import Blog from '../model/model'
+import errorHandler from '../utils/error'
 
-
-
-class BlogController {
-  static async getAllBlogs(req: Request, res: Response){
+export default class BlogController {
+  static async getAllBlogs(_req: Request, res: Response) {
     try {
-      const blogs = await Blog.getAll()
-      res
+      res.json(await Blog.getAll())
     } catch (error) {
-      error
+      errorHandler(res)
     }
+  }
 
+  static async getBlogById(req: Request, res: Response) {
+    try {
+      const { id } = req.params
+      const blog = await Blog.getById(id)
+      blog 
+        ? res.json(blog) 
+        : errorHandler(res, 404, 'Blog not found!')
+    } catch (error) {
+      errorHandler(res)
+    }
+  }
+
+  static async createBlog(req: Request, res: Response) {
+    try {
+      const newBlog = Blog.create(req.body)
+      res.status(201).json(newBlog)
+    } catch (error) {
+      errorHandler(res)
+    }
+  }
+
+  static async updateBlog(req: Request, res: Response) {
+    try {
+      const { id } = req.params
+      const updatedBlog = Blog.update(id, req.body)
+      updatedBlog
+        ? res.json(updatedBlog)
+        : errorHandler(res, 404, 'Blog not found!')
+    } catch (error) {
+      errorHandler(res)
+    }
   }
 }
