e431ce4bb62906913d9e6fad0a85a25b0a336279
kaypa <jose_kaypa@hotmail.com>
2023-10-16 15:48:39 -0500

    header transitions

--- frontend/src/components/Blog.tsx
+++ frontend/src/components/Blog.tsx
@@ -6,7 +6,7 @@ export default function Home({id, image, title, desc}: {id:string, image: string
       <div className="bg-slate-300 md:min-w-[500px] md:min-h-[200px] md:aspect-[2/1]">
         <img src={image} className="md:w-full md:h-full md:object-cover" />
       </div>
-      <div className="h-full flex flex-col justify-around gap-3">
+      <div>
         <div>
           <h2 className="text-zinc-900 font-bold md:text-xl md:mb-2">{title}</h2>
           <p className="text-zinc-900 text-xs md:text-sm">{desc}</p>
