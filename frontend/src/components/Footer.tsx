736ece1467e87ed9f40dfd2bcb47c74ef3922aea
kaypa <jose_kaypa@hotmail.com>
2023-10-17 10:53:49 -0500

    footer done

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,13 @@
-import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiOutlineLinkedin, AiOutlineHeart} from 'react-icons/ai'
 
 
 export default function Footer() {
   return (
     <footer className="flex flex-col justify-around items-center bg-black text-white h-52 mt-5">
       <p>Travelling Nerd</p>
-      <p>Made with love by Jose Kaypa</p>
-      <AiOutlineLinkedin className="text-3xl" />
-      <a>jose_kaypa@hotmail.com</a>
+      <a href="https://www.linkedin.com/in/jkaypa/" target='_blank' rel='noopener noreferrer'><AiOutlineLinkedin className="text-3xl" /></a>
+      <a href='mailto:jose_kaypa@hotmail.com?subject=Hello&body=Hi there Kaypa'>jose_kaypa@hotmail.com</a>
+      <p className='flex items-center'>Made with<AiOutlineHeart className='w-8'/>by Jose Kaypa</p>
     </footer>
   )
 }
