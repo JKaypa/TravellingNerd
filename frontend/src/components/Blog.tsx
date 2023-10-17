ecd8b7ae046ca33e1e35df19fe20f526eacb5d44
kaypa <jose_kaypa@hotmail.com>
2023-10-17 10:27:21 -0500

    syles

--- frontend/src/components/Blog.tsx
+++ frontend/src/components/Blog.tsx
@@ -8,10 +8,10 @@ export default function Home({id, image, title, desc}: {id:string, image: string
       </div>
       <div>
         <div>
-          <h2 className="text-zinc-900 font-bold md:text-xl md:mb-2">{title}</h2>
+          <Link to={`/detail/${id}`} className="text-zinc-900 font-bold md:text-xl md:mb-2">{title}</Link>
           <p className="text-zinc-900 text-xs md:text-sm">{desc}</p>
         </div>
-        <Link to={`/detail/${id}`} className="">
+        <Link to={`/detail/${id}`} className="bg-zinc-200 text-sm p-1">
           Read more
         </Link>
       </div>
